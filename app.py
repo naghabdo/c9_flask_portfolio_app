712d951244e0a93a3f98d2daa23b4634391bc54a
naghabdo <70665605+naghabdo@users.noreply.github.com>
2020-09-04 00:59:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ def add_numbers_post():
   	      print(request.form['text'].split())
   	      total = 0
   	      try:
-                for str_num in request.form['text'].split():
-  	            total += int(str_num)
-  	        return render_template('add_numbers.html', result=str(total))
+                  for str_num in request.form['text'].split():
+  	              total += int(str_num)
+  	          return render_template('add_numbers.html', result=str(total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
